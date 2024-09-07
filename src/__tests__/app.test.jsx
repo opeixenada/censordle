2083c9bd568e0b94a336459c719f76e1e7863047
Anna Yudina <annayudi@gmail.com>
2024-09-07 17:14:13 +0200

    Tests update

--- src/__tests__/app.test.jsx
+++ src/__tests__/app.test.jsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 import {render, screen} from '@testing-library/react'
-import App from '../src/app';
+import App from '../App';
 
 describe('App', () => {
     it('renders Censordle title', () => {
