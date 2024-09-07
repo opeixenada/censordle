916e0da23715ad60af56cab3328de9f03b7b6222
Anna Yudina <annayudi@gmail.com>
2024-09-07 16:44:32 +0200

    Migrate to Next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import '../index.css'
+
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Censordle',
+    description: 'The game of Censordle',
+}
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+        <body>
+        <div id="root">{children}</div>
+        </body>
+        </html>
+    )
+}
