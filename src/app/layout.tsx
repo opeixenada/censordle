2cb6f6672c79cd73f536e729eb4dd32cbdc6fa2e
Anna Yudina <annayudi@gmail.com>
2024-09-07 19:02:16 +0200

    Move manifest to /public folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import type {Metadata} from 'next'
 export const metadata: Metadata = {
     title: 'Censordle',
     description: 'The game of Censordle',
-    manifest: './manifest.json',
+    manifest: 'manifest.json',
 }
 
 export default function RootLayout({
